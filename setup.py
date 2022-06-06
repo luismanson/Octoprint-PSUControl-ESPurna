871438c649e59a72555317fa01367c2a273c8e5f
Luis D <luismanson@users.noreply.github.com>
2022-06-06 14:38:29 -0300

    new release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ plugin_package = "octoprint_psucontrol_espurna"
 plugin_name = "Octoprint-PSUControl-ESPurna"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.1"
+plugin_version = "0.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
